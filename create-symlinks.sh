385a459deee0c2595ac91b7f8cb4ec008f3dd365
Denys Vitali <denys@denv.it>
2018-04-28 17:22:50 +0200

    Add Kitty symlink, re-add zshrc symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/vimrc ~/.vimrc
-ln -s $DIR/i3/config ~/.config/i3/config
+ln -s $DIR/zshrc ~/.zshrc
+ln -s $DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
