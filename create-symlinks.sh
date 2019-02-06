7aa6e896e54381c36c88c06251d3961e721bc9c2
Denys Vitali <denys@denv.it>
2019-02-06 20:59:13 +0100

    Add fontconfig

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,7 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s $DIR/alacritty.yml ~/.config/alacritty/alacritty.yml
-ln -s $DIR/i3/config ~/.config/i3/config
+ln -s $DIR/alacritty.yaml ~/.config/alacritty/alacritty.yml
 ln -s $DIR/i3blocks.conf ~/.i3blocks.conf
-ln -s $DIR/rofi-config ~/.config/rofi
+ln -s $DIR/fonts.conf ~/.config/fontconfig/
+
+
