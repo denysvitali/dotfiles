14bdf4f6cf4ec505ec79c18e1249dcc4b7d20145
Denys Vitali <denys@denv.it>
2019-02-06 20:30:47 +0100

    Add i3blocks

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,4 +3,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s $DIR/alacritty.yaml ~/.config/alacritty/alacritt.yml
+ln -s $DIR/alacritty.yaml ~/.config/alacritty/alacritty.yml
+ln -s $DIR/i3blocks.conf ~/.i3blocks.conf
+
