34ac39860120909f0af02f151ef60dfbfe66d5d6
Denys Vitali <denys@denv.it>
2019-02-06 21:31:30 +0100

    Add i3 symlink

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,6 +5,7 @@ ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -s $DIR/alacritty.yaml ~/.config/alacritty/alacritty.yml
 ln -s $DIR/i3blocks.conf ~/.i3blocks.conf
+ln -s $DIR/i3/config ~/.config/i3/config
 ln -s $DIR/fonts.conf ~/.config/fontconfig/
 ln -s $DIR/dunstrc ~/.config/dunst/dunstrc
 ln -s $DIR/.gtkrc-2.0 ~/.gtkrc-2.0
