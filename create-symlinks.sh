d68649153b235b76963b6622da8af8aaeebf140a
Denys Vitali <denys@denv.it>
2019-02-06 20:31:31 +0100

    Link i3 config

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -3,4 +3,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/zshrc ~/.zshrc
 ln -s $DIR/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s $DIR/alacritty.yaml ~/.config/alacritty/alacritt.yml
+ln -s $DIR/alacritty.yaml ~/.config/alacritty/alacritty.yml
+ln -s $DIR/i3/config ~/.config/i3/config
